2f9b4e74954b1ba50f30b2a83c7a5e3ac85faafb
yousufkhan13 <khanzzyyy@gmail.com>
2024-02-05 12:37:52 +0500

    answer18

---
+++ question18.dart
@@ -0,0 +1,13 @@
+void main() {
+  String username1 = "khanzzyyyy";
+  String password = "asdfasdf";
+  String username2 = "lalita";
+  String password1 = "laila";
+
+  if (username1 == 'khanzzyyyy' && password == 'asdfasdf') {
+    print('login successful');
+  } else if (username2 == 'toto' && password == 'laila') {
+    print('again enter');
+  } else
+    ('login failed');
+}
