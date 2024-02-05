0aa1eb8e9e16666d86e68e73858c807b8c6c8aa7
yousufkhan13 <khanzzyyy@gmail.com>
2024-02-05 12:28:49 +0500

    answer17

---
+++ question17.dart
@@ -0,0 +1,11 @@
+void main() {
+  String username = "khanzzyyy";
+
+  String password = "12345678";
+
+  if (username == "khanzzyyy" && password == '12345678') {
+    print('login successful');
+  } else {
+    print('enter correct credentials');
+  }
+}
