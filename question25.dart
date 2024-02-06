04b4b5f9734f52a94fee00dea34c91b39e7345fc
yousufkhan13 <khanzzyyy@gmail.com>
2024-02-06 21:50:02 +0500

    answer25

--- question25.dart
+++ question25.dart
@@ -1,18 +1,55 @@
 void main() {
-  List numbers = [2, 4, 3];
-  int shankar = 23;
-  List prime = [];
-  for (var i in numbers) {
-    if (i % numbers == 0) {
-      prime.add(i);
-      print(prime);
-    } else if (i % 2 == 0) {
-      print('not');
-    }
-  }
+  List numbers1 = [3, 2, 4, 5, 6, 7, 8];
+
+  getPrimeNo(numbers1);
 }
 
+getPrimeNo(List numbers) {
+  List primeNumbers = [];
+  for (var i in numbers) {
+    if (primo(i)) {
+      primeNumbers.add(i);
+    }
+  }
+  print(primeNumbers);
+}
 
+bool primo(int number) {
+  if (number <= 1) {
+    return false;
+  }
 
-//  primeornot(numbers: [31, 23, 4]);
+  for (var i = 2; i < number; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
+// void main() {
+//   // declare list of integers
+//   var myList = [2, 3, 4, 7, 5, 12, 11, 15, 21, 19, 20];
+//   // Invoke the getPrimeNo() function
+//   getPrimeNo(myList);
+// }
+
+// // A function that displays the list of prime number
+// getPrimeNo(List numbers) {
+//   List primeNumbers = [];
+//   for (int val in numbers) {
+//     if (isPrime(val)) {
+//       primeNumbers.add(val);
+//     }
+//   }
+//   print('The prime numbers in the List: $primeNumbers');
+// }
+
+// // A function that checks if a number is prime or not
+// bool isPrime(int number) {
+//   if (number <= 1) return false; // Prime number is >
+//   for (int i = 2; i < number; i++) {
+//     if (number % i == 0) return false;
+//   }
+//   return true;
+// }
